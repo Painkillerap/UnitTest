de6b482315c86ea8c8b0f0b1a626a55a116635c4
Painkillerap <painkiller.ap@yandex.ru>
2023-10-30 18:33:36 +0300

    Seminar5,HW5

---
+++ src/main/java/seminar5/Restoran/Kitchen.java
@@ -0,0 +1,8 @@
+package seminar5.Restoran;
+
+// Класс, представляющий кухню
+public class Kitchen {
+    public void prepareOrder(Order order) {
+        // Здесь реализована логика приготовления заказа
+    }
+}
---
+++ src/main/java/seminar5/Restoran/Order.java
@@ -0,0 +1,16 @@
+package seminar5.Restoran;
+import java.util.ArrayList;
+import java.util.List;
+
+// Класс, представляющий заказ
+public class Order {
+    private List<MenuItem> items = new ArrayList<>();
+
+    public void addItem(MenuItem item) {
+        items.add(item);
+    }
+
+    public List<MenuItem> getItems() {
+        return items;
+    }
+}
